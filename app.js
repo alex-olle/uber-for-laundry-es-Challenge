98729a69cb8484cc3bb403b0df939d19405b9b77
Alex Olle <alex.olle.93@gmail.com>
2020-10-28 12:40:24 +0100

    Iteration 4 done

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ mongoose
   });
 
 const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth')
+const authRouter = require('./routes/auth');
+const laundryRouter = require('./routes/laundry');
 
 const app = express();
 
@@ -44,6 +45,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.use('/', indexRouter);
 app.use('/', authRouter);
+app.use('/', laundryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/laundry.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+
+const withAuth = require("../helpers/middleware");
+
+const User = require("../models/user");
+
+router.get("/dashboard", withAuth, async (req, res, next) => {
+  if (req.userID) {
+    try {
+      const userUpdated = await User.findById(req.userID);
+
+      res.locals.currentUserInfo = userUpdated;
+
+      res.render("laundry/dashboard");
+    } catch (error) {
+      next(error);
+      return;
+    }
+  } else {
+    res.redirect("/");
+  }
+});
+
+router.post("/launderers", withAuth, async (req, res, next) => {
+  const userID = req.userID;
+
+  const laundererInfo = {
+    fee: req.body.fee,
+    isLaunderer: true,
+  };
+
+  try {
+    const theUser = await User.findByIdAndUpdate(userID, laundererInfo, {
+      new: true,
+    });
+
+    req.user = theUser;
+
+    res.redirect("/dashboard");
+  } catch (error) {
+    next(error);
+    return;
+  }
+});
+
+module.exports = router;
